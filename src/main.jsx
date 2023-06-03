e7893072152bcf1a38b4c56dadaf1db8a1135cf9
Mausam Pun <sonicmausam@gmail.com>
2023-06-03 16:07:51 +0545

    store err

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './app/Store.jsx'
+import { store } from './services/store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
